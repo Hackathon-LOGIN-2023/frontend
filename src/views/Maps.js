27203af5a9443436b96e7d3ab3b7c741062282a8
Gustavo Soares <gusnsoares@gmail.com>
2023-01-10 11:43:06 +0100

    first map

--- src/views/Maps.js
+++ src/views/Maps.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import {Dimensions} from 'react-native';
 
 export default function Maps() {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Map View</Text>
-      </View>
+      <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={{
+          height: Dimensions.get('window').height,
+          width: Dimensions.get('window').width,
+        }}>
+        <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}} />
+      </MapView>
     </SafeAreaView>
   );
 }
