86832af28dfc6551a38fd5f9cf89c3a70f5b2c3a
Gustavo Soares <gusnsoares@gmail.com>
2023-01-13 17:44:55 +0100

    improve issue detail page visual

--- src/views/IssueDetail.js
+++ src/views/IssueDetail.js
@@ -4,12 +4,14 @@ import {
   Button,
   Dimensions,
   Image,
+  ScrollView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 import useIssue from '../hooks/useIssue';
 import {URI_IMAGE} from '../consts/backend';
+import {CHOICES} from '../consts/multiplechoice';
 
 const styles = StyleSheet.create({
   image: {
@@ -17,13 +19,28 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     resizeMode: 'contain',
   },
+  title: {
+    color: '#000000',
+    fontSize: 40,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  label: {
+    color: '#000000',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  value: {
+    color: '#000000',
+    fontSize: 18,
+  },
 });
 
 export default function IssueDetail({navigation, route}) {
   const {issueId} = route.params;
   const {data: issue, isLoading, isSuccess} = useIssue({issueId});
   const [imgRatio, setImgRatio] = useState(0);
-  const width = Dimensions.get('window').width;
+  const {width, height} = Dimensions.get('window');
 
   useLayoutEffect(function () {
     if (isSuccess) {
@@ -57,8 +74,8 @@ export default function IssueDetail({navigation, route}) {
   });
 
   return (
-    <View>
-      {/*{issue.image && <Image source={{uri: issue.image}} style={styles.image} />}*/}
+    <ScrollView style={{backgroundColor: '#FFF', height}}>
+      <Text style={styles.title}>{issue.title}</Text>
       {issue.image && (
         <Image
           source={{uri: `${URI_IMAGE}${issue.image}`}}
@@ -69,19 +86,25 @@ export default function IssueDetail({navigation, route}) {
           }}
         />
       )}
-      <Text>Id: {issue._id}</Text>
-      <Text>Issue: {issue.title}</Text>
-      <Text>Description: {issue.description}</Text>
-      <Text>Date: {issue.date}</Text>
-      <Text>Status: {issue.status}</Text>
-      <Text>Severity: {issue.severity}</Text>
-      <Text>Category: {issue.category}</Text>
-      <View>
+      <Field label="Description:" value={issue.description} />
+      <Field label="Date:" value={issue.date} />
+      <Field label="Severity:" value={CHOICES.severity[issue.severity - 1]} />
+      <Field label="Category:" value={CHOICES.category[issue.category - 1]} />
+      {/* <View>
         <Text>Votes</Text>
         {issue.votes.map(vote => (
           <Text key={`vote--${vote._id}`}>{vote._id}</Text>
         ))}
-      </View>
+      </View> */}
+    </ScrollView>
+  );
+}
+
+function Field({label, value}) {
+  return (
+    <View style={{paddingTop: 10}}>
+      <Text style={styles.label}>{label}</Text>
+      <Text style={styles.value}>{value}</Text>
     </View>
   );
 }
